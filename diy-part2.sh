f68ce3a1d712498138f662f6250273fe0203b373
lisr <2390024566@qq.com>
2025-06-29 22:35:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,8 +20,11 @@
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 
-
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/ophub/luci-app-amlogic.git package/amlogic
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
 rm -rf package/feeds/packages/shadowsocks-libev
 git clone https://github.com/msdos03/openwrt-package-shadowsocks-libev.git package/shadowsocks-libev
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
