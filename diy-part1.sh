75c24a3f5496b840911697cba71c9487a1157b4a
lisr <2390024566@qq.com>
2025-06-29 18:20:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git shadowsocks-rust https://github.com/shadowsocks/shadowsocks-rust.git' >>feeds.conf.default
-echo 'src-git luci-theme-argon https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
+
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git shadowsocks-rust https://github.com/shadowsocks/shadowsocks-rust.git' >>feeds.conf.default
+#echo 'src-git luci-theme-argon https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
